aaa883d0f5a854ac465a4ae58e57170fea25baaf
wilterd <wilterd18@gmail.com>
2023-10-24 08:41:50 -0400

    sin errores tablas, falta revision

--- taller.sql
+++ taller.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 CREATE TABLE competiciones (
   id_competicion INT PRIMARY KEY,
   nombre VARCHAR(50),
@@ -6,35 +8,55 @@ CREATE TABLE competiciones (
   temporada VARCHAR(10) NOT NULL
 );
 
+CREATE TABLE federaciones (
+    id_federacion INT PRIMARY KEY,
+    nombre_federacion VARCHAR(255) NOT NULL,
+    fecha_creacion DATE NOT NULL
+);
+
 CREATE TABLE clubes (
   id_club INT PRIMARY KEY,
   nombre_club VARCHAR(50) NOT NULL,
-  fecha_creacion DATE NOT NULL,
   num_socios INT NOT NULL,
   id_entrenador INT,
   id_federacion INT,
-  FOREIGN KEY (id_entrenador) REFERENCES entrenadores(id_entrenador),
+	
   FOREIGN KEY (id_federacion) REFERENCES federaciones(id_federacion)
-);
-
-CREATE TABLE jugadores (
-  id_jugador INT PRIMARY KEY,
-  nombre VARCHAR(50),
-  apellido VARCHAR(50),
-  fecha_nacimiento DATE,
-  posicion VARCHAR(50),
-  id_club INT,
-  FOREIGN KEY (id_club) REFERENCES clubes(id_club)
 );
 
 CREATE TABLE entrenadores (
   id_entrenador INT PRIMARY KEY,
-  nombre VARCHAR(50),
-  apellido VARCHAR(50),
-  fecha_nacimiento DATE,
+  nombre VARCHAR(50) NOT NULL,
+  direccion VARCHAR(100) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  fecha_inicio_carrera DATE NOT NULL,
   id_club INT,
   FOREIGN KEY (id_club) REFERENCES clubes(id_club)
 );
+
+CREATE TABLE especialidades (
+  id_especialidad INT PRIMARY KEY,
+  nombre_especialidad VARCHAR(50) NOT NULL,
+  grado_especialidad INT NOT NULL
+);
+
+
+CREATE TABLE jugadores (
+  id_jugador INT PRIMARY KEY,
+  nombre VARCHAR(50),
+  fecha_nacimiento DATE NOT NULL,
+  posicion VARCHAR(50),
+  id_club INT,
+  telefono VARCHAR(20) NOT NULL,
+  peso FLOAT NOT NULL,
+  altura FLOAT NOT NULL,
+  id_especialidad INT,
+  FOREIGN KEY (id_club) REFERENCES clubes(id_club),
+  FOREIGN KEY (id_especialidad) REFERENCES especialidades(id_especialidad)
+);
+
+
 
 -- Tabla de clasificaciones
 CREATE TABLE clasificaciones (
@@ -45,3 +67,6 @@ CREATE TABLE clasificaciones (
   FOREIGN KEY (id_club) REFERENCES clubes(id_club),
   FOREIGN KEY (id_competicion) REFERENCES competiciones(id_competicion)
 );
+
+COMMIT;
+
