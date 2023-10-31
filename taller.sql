99a2db1da2fcfdde51f105aa8ec35e8530c3af8b
wilterd <wilterd18@gmail.com>
2023-10-30 23:30:46 -0400

    pdf

--- taller.sql
+++ taller.sql
@@ -1,132 +1,118 @@
 BEGIN;
 
-CREATE TABLE competiciones (
-  id_competicion SERIAL PRIMARY KEY,
-  nombre VARCHAR(50),
-  fecha_inicio DATE,
-  fecha_fin DATE,
-  temporada VARCHAR(10) NOT NULL
+CREATE TABLE FEDERACIONES(
+    nombre VARCHAR(30) PRIMARY KEY,
+    fechaCreacion DATE NOT NULL
 );
 
-CREATE TABLE partidos (
-    id_partido SERIAL PRIMARY KEY,
-    id_club_local int NOT NULL,
-    id_club_visitante int NOT NULL,
-    fecha_creacion DATE NOT NULL
+CREATE TABLE CLUBES (
+    codigoClub INTEGER PRIMARY KEY,
+    nombreClub VARCHAR(30) NOT NULL,
+    nroSocios INTEGER NOT NULL,
+    nombreFederacion VARCHAR(30),
+    CONSTRAINT fk_federaciones FOREIGN KEY (nombreFederacion) REFERENCES FEDERACIONES (nombre)
 );
 
-CREATE TABLE federaciones (
-    id_federacion SERIAL PRIMARY KEY,
-    nombre_federacion VARCHAR(255) NOT NULL,
-    fecha_creacion DATE NOT NULL
+CREATE TABLE CLASIFICACIONES(
+    codigoClub INTEGER NOT NULL,
+    temporada VARCHAR(30) NOT NULL,
+    competicion VARCHAR(30) NOT NULL,
+    posicion INTEGER DEFAULT 0,
+    CONSTRAINT pk_clasificaciones PRIMARY KEY(codigoClub,temporada,competicion),
+    CONSTRAINT fk_club FOREIGN KEY (codigoClub) REFERENCES CLUBES(codigoClub)
 );
 
-CREATE TABLE clubes (
-  id_club SERIAL PRIMARY KEY,
-  nombre_club VARCHAR(50) NOT NULL,
-  num_socios INT NOT NULL,
-  id_entrenador INT,
-  id_federacion INT,
-	
-  FOREIGN KEY (id_federacion) REFERENCES federaciones(id_federacion)
+CREATE TABLE JUEGOSGANADOS(
+    codigoClub INTEGER NOT NULL,
+    temporada VARCHAR(30) NOT NULL,
+    competicion VARCHAR(30) NOT NULL,
+    cantidad INTEGER DEFAULT 0,
+    CONSTRAINT pk_juegosGanados PRIMARY KEY(codigoClub,temporada,competicion),
+    CONSTRAINT fk_clubes FOREIGN KEY (codigoClub) REFERENCES CLUBES(codigoClub)
 );
 
-CREATE TABLE entrenadores (
-  id_entrenador SERIAL PRIMARY KEY,
-  nombre VARCHAR(50) NOT NULL,
-  direccion VARCHAR(100) NOT NULL,
-  telefono VARCHAR(20) NOT NULL,
-  fecha_nacimiento DATE NOT NULL,
-  fecha_inicio_carrera DATE NOT NULL,
-  id_club INT,
-  FOREIGN KEY (id_club) REFERENCES clubes(id_club)
+CREATE TABLE EMPLEADOS(
+    codigo SERIAL PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+    fechaNac DATE NOT NULL,
+    direccion VARCHAR(30) NOT NULL,
+    numeroTlf INTEGER NOT NULL,
+    tipo VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE especialidades (
-  id_especialidad SERIAL PRIMARY KEY,
-  nombre_especialidad VARCHAR(50) NOT NULL,
-  grado_especialidad INT NOT NULL
+CREATE TABLE ENTRENADORES (
+    codigoEmp INTEGER PRIMARY KEY,
+    fueJugador BOOlEAN DEFAULT FALSE,
+    fechaInicio DATE NOT NULL,
+    CONSTRAINT fk_empEnt FOREIGN KEY(codigoEmp) REFERENCES EMPLEADOS(codigo)
 );
 
-CREATE TABLE jugadores (
-  id_jugador SERIAL PRIMARY KEY,
-  nombre VARCHAR(50),
-  fecha_nacimiento DATE NOT NULL,
-  posicion VARCHAR(50),
-  id_club INT,
-  telefono VARCHAR(20) NOT NULL,
-  peso FLOAT NOT NULL,
-  altura FLOAT NOT NULL,
-  id_especialidad INT,
-  FOREIGN KEY (id_club) REFERENCES clubes(id_club),
-  FOREIGN KEY (id_especialidad) REFERENCES especialidades(id_especialidad)
+CREATE TABLE JUGADORES (
+    codigoEmp INTEGER PRIMARY KEY,
+    peso FLOAT NOT NULL,
+    altura FLOAT NOT NULL,
+    CONSTRAINT fk_empJug FOREIGN KEY(codigoEmp) REFERENCES EMPLEADOS(codigo)
+);
+
+CREATE TABLE ESPECIALIDADES (
+    codigoJugador INTEGER NOT NULL,
+    nombreEsp VARCHAR(50) NOT NULL,
+    gradoDominio INT NOT NULL,
+    CONSTRAINT fk_jugadores FOREIGN KEY (codigoJugador) REFERENCES JUGADORES(codigoEmp)
+);
+
+CREATE TABLE OFERTAN (
+    codigoEmpleado INTEGER NOT NULL,
+    codigoClub INTEGER NOT NULL,
+    fechaOferta DATE NOT NULL,
+    importe FLOAT NOT NULL,
+    CONSTRAINT pk_ofertan PRIMARY KEY(codigoEmpleado,codigoClub,fechaOferta),
+    CONSTRAINT fk_empleado FOREIGN KEY (codigoEmpleado) REFERENCES EMPLEADOS(codigo),
+    CONSTRAINT fk_codigo_club FOREIGN KEY (codigoClub) REFERENCES CLUBES(codigoClub)
+);
+
+CREATE TABLE COMPITEN (
+    pClub INTEGER NOT NULL,
+    sClub INTEGER NOT NULL,
+    temporada VARCHAR(30) NOT NULL,
+    competicion VARCHAR(30) NOT NULL,
+    clubGanador INTEGER NOT NULL,
+    CONSTRAINT pk_compiten PRIMARY KEY(pClub,sClub,temporada,competicion,clubGanador),
+    CONSTRAINT fk_pClub FOREIGN KEY (pClub) REFERENCES CLUBES(codigoClub),
+    CONSTRAINT fk_sClub FOREIGN KEY (sClub) REFERENCES CLUBES(codigoClub)
+);
+
+CREATE TABLE CONTRATAN (
+    codigoEmpleado INTEGER NOT NULL,
+    codigoClub INTEGER NOT NULL,
+    fechaBaja DATE NOT NULL,
+    importe FLOAT NOT NULL,
+    CONSTRAINT pk_contratan PRIMARY KEY(codigoEmpleado,codigoClub,fecha_baja),
+    CONSTRAINT fk_emp FOREIGN KEY (codigoEmpleado) REFERENCES EMPLEADOS(codigo),
+    CONSTRAINT fk_cod_club FOREIGN KEY (codigoClub) REFERENCES CLUBES(codigoClub)
 );
 
 
--- Tabla de clasificaciones
-CREATE TABLE clasificaciones (
-  id_clasificacion SERIAL PRIMARY KEY,
-  id_club INT,
-  id_competicion INT,
-  posicion INT NOT NULL,
-  FOREIGN KEY (id_club) REFERENCES clubes(id_club),
-  FOREIGN KEY (id_competicion) REFERENCES competiciones(id_competicion)
-);
+-- Crear un trigger que actualice la cantidad de juegos ganados de un club cuando se ingresa un resultado en COMPITEN.
+CREATE OR REPLACE FUNCTION actualizarJuegosGanados() RETURNS TRIGGER AS $$
+BEGIN
+    -- Si el clubGanador es el primer club (pClub), se incrementa en 1 la cantidad de juegos ganados.
+    IF NEW.clubGanador = NEW.pClub THEN
+        UPDATE JUEGOSGANADOS
+        SET cantidad = cantidad + 1
+        WHERE codigoClub = NEW.clubGanador AND temporada = NEW.temporada AND competicion = NEW.competicion;
+    END IF;
 
+    -- Si el clubGanador es el segundo club (sClub), no se modifica la cantidad de juegos ganados.
 
-CREATE TABLE contrataciones (
-  id_contratacion SERIAL PRIMARY KEY,
-  id_persona INT,
-  ic_club INT,
-  fecha_contratacion DATE NOT NULL,
-  fecha_baja DATE,
-  importe DECIMAL(10, 2),
-  FOREIGN KEY (id_persona) REFERENCES jugadores(id_jugador) ON DELETE CASCADE,
-  FOREIGN KEY (id_persona) REFERENCES entrenadores(id_entrenador) ON DELETE CASCADE,
-  FOREIGN KEY (id_club) REFERENCES clubes(id_club)
-);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE TABLE ofertas (
-  id_oferta SERIAL PRIMARY KEY,
-  id_persona INT, 
-  id_club INT,
-  fecha_oferta DATE NOT NULL,
-  importe DECIMAL(10, 2), 
-  FOREIGN KEY (id_persona) REFERENCES jugadores(id_Jugador) ON DELETE CASCADE,
-  FOREIGN KEY (id_persona) REFERENCES entrenadores(id_entrenador) ON DELETE CASCADE,
-  FOREIGN KEY (id_club) REFERENCES clubes(id_club)
-);
-
--- TRIGGERS
-
--- CREATE OR REPLACE FUNCTION registro_contratacion()
--- RETURNS TRIGGER AS $$
--- BEGIN
---   INSERT INTO contrataciones (id_persona, id_club, fecha_contratacion, importe)
---   VALUES (NEW.id_persona, NEW.id_club, NEW.fecha_contratacion, NEW.importe);
---   RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- CREATE TRIGGER registro_contratacion_trigger
--- AFTER INSERT ON contrataciones_temp
--- FOR EACH ROW
--- EXECUTE FUNCTION registro_contratacion();
-
--- CREATE OR REPLACE FUNCTION registro_oferta()
--- RETURNS TRIGGER AS $$
--- BEGIN
---   INSERT INTO ofertas (id_persona, id_club, fecha_oferta, importe)
---   VALUES (NEW.id_persona, NEW.id_club, NEW.fecha_oferta, NEW.importe);
---   RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- CREATE TRIGGER registro_oferta_trigger
--- AFTER INSERT ON ofertas
--- FOR EACH ROW
--- EXECUTE FUNCTION registro_oferta();
-
+-- Crear el trigger que ejecutará la función al insertar o actualizar la tabla COMPITEN.
+CREATE TRIGGER trig_actualizar_juegosganados
+AFTER INSERT OR UPDATE ON COMPITEN
+FOR EACH ROW
+EXECUTE FUNCTION actualizarJuegosGanados;
 
 COMMIT;
-
